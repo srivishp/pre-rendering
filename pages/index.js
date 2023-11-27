5a87e8450f94a94d86f5e4a43cc92aa1bb96f799
Sri Vishnu <srivishp@gmail.com>
2023-11-27 12:06:36 +0530

    Add context and redirect

--- pages/index.js
+++ pages/index.js
@@ -16,7 +16,8 @@ function HomePage(props) {
 }
 
 // Code written inside getStaticProps() will not be executed on client side
-export async function getStaticProps() {
+export async function getStaticProps(context) {
+  // context contains the data we want to show
   // process object is a global object by Node JS
   //-> cwd = current working directory
   //* Gives the cwd of this file when it is executed
@@ -27,6 +28,19 @@ export async function getStaticProps() {
   // parse json into a regular object
   const data = JSON.parse(jsonData);
 
+  // redirecting when there is no data
+  if (!data) {
+    return {
+      redirect: {
+        destination: "/no-data",
+      },
+    };
+  }
+
+  // condition for product not found
+  if (data.products.length === 0) {
+    return { notFound: true };
+  }
   return {
     props: {
       products: data.products,
