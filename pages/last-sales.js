f0d93bc4ec1e64609e0c5723b11d9498fe176c1d
Sri Vishnu <srivishp@gmail.com>
2024-01-18 18:50:53 +0530

    Implement Stale While Revalidate - useSWR() - strategy

--- pages/last-sales.js
+++ pages/last-sales.js
@@ -1,40 +1,73 @@
 // Client Side data fetching
 
-const { useEffect, useState } = require("react");
+import { useEffect, useState } from "react";
+import useSWR from "swr";
 
 function LastSalesPage() {
   const [sales, setSales] = useState();
-  const [isLoading, setIsLoading] = useState(false);
+  // const [isLoading, setIsLoading] = useState(false);
+
+  //-> Stale While Revalidate
+  //# Cache Invalidation Strategy
+  // SWR is a strategy to first return the data from cache (stale),
+  // then send the fetch request (revalidate),
+  // and finally come with the up-to-date data.
+  //? https://swr.vercel.app/
+
+  const { data, error } = useSWR(
+    "https://client-side-fetching-nextjs-default-rtdb.asia-southeast1.firebasedatabase.app/sales.json",
+    (url) => fetch(url).then((res) => res.json())
+  );
+
   useEffect(() => {
-    setIsLoading(true);
-    fetch(
-      "https://client-side-fetching-nextjs-default-rtdb.asia-southeast1.firebasedatabase.app/sales.json"
-    )
-      .then((response) => response.json())
-      .then((data) => {
-        const transformedSales = [];
+    if (data) {
+      const transformedSales = [];
 
-        for (const key in data) {
-          transformedSales.push({
-            id: key,
-            username: data[key].username,
-            item: data[key].item,
-            volume: data[key].volume,
-          });
-        }
+      for (const key in data) {
+        transformedSales.push({
+          id: key,
+          username: data[key].username,
+          item: data[key].item,
+          volume: data[key].volume,
+        });
+      }
+      setSales(transformedSales);
+    }
+  }, [data]);
+  //   useEffect(() => {
+  //     setIsLoading(true);
+  //     fetch(
+  //       "https://client-side-fetching-nextjs-default-rtdb.asia-southeast1.firebasedatabase.app/sales.json"
+  //     )
+  //       .then((response) => response.json())
+  //       .then((data) => {
+  //         const transformedSales = [];
 
-        setSales(transformedSales);
-        setIsLoading(false);
-      });
-  }, []);
+  //         for (const key in data) {
+  //           transformedSales.push({
+  //             id: key,
+  //             username: data[key].username,
+  //             item: data[key].item,
+  //             volume: data[key].volume,
+  //           });
+  //         }
 
-  if (isLoading) {
+  //         setSales(transformedSales);
+  //         setIsLoading(false);
+  //       });
+  //   }, []);
+
+  if (error) {
+    return <p>Failed to load</p>;
+  }
+
+  if (!data || !sales) {
     return <p>Loading...</p>;
   }
 
-  if (!sales) {
-    return <p>No data yet</p>;
-  }
+  //   if (!sales) {
+  //     return <p>No data yet</p>;
+  //   }
 
   return (
     <ul>
